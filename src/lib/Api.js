70e1660eedaa98af47ada72fd0ea49bc862c0501
nikolaykolibarov <nikolaykolibarov@gmail.com>
2016-12-05 23:40:45 +0200

    Added Public API key, updated service url, added .idea to .gitignore

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -3,12 +3,13 @@ import Requester from './Requester';
 export default class Api {
     constructor() {
         this._marvelUrl = 'https://gateway.marvel.com';
+        this._marvelKey = '5b2cf74d7b537105c93b55478200435a';
         this._requester = Requester.request;
         this._characters = null;
     }
 
     getCharacters () {
-        this._requester(this._marvelUrl + '/v1/public/characters', {}, (error, result) => {
+        this._requester(this._marvelUrl + '/v1/public/characters?apikey=' + this._marvelKey, {}, (error, result) => {
             if (typeof error === 'undefined') error = null;
             if (typeof result === 'undefined') result = null;
 
